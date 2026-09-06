26c93bcfb6a16efc00a586b570b9f309a6683cfc
qynwphuu <qynwphuu@gmail.com>
2026-09-06 22:20:39 +0300

    Refactor & Style: Update CSS, remove default Vite style and improve layout structure

--- hh-riski/src/App.tsx
+++ hh-riski/src/App.tsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom";
 import "./App.css"
 function App() {
   return (
-    <div>
-      <Outlet />
+    <div className="app-shell">
+      <main className="app-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
