696440cb6a4b7d0a598510b64c9bf15f5e1faefd
Veera Virtanen <veeraeila16@gmail.com>
2026-09-25 12:41:07 +0300

    Fix: parse token for risk calculation request

--- hh-riski/src/services/riskCalculation.ts
+++ hh-riski/src/services/riskCalculation.ts
@@ -3,11 +3,13 @@ import type { RiskCalculationRequest, RiskCalculationResponse } from "../types";
 const url = import.meta.env.VITE_BACKEND_URL;
 
 export async function calculateRisk(answers: RiskCalculationRequest, token: string) {
+    const parsedToken = token ? JSON.parse(token) : null
+    
     const response = await fetch (`${url}/calculaterisk`, {
         method: "POST",
         headers: { 
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
+            "Authorization": `Bearer ${parsedToken}`
         },
         body: JSON.stringify(answers)
     });
